1b19b939b23b8b7be13a60d7d8f6ee2ec4082c2a
Eddo van den Boom <eddovdboom@gmail.com>
2020-09-08 14:15:11 +0200

    Added unit test for scanning bug

---
+++ TosrGui.Test/ScanningBugTest.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Tosr;
+using Xunit;
+
+namespace TosrGui.Test
+{
+    public class ScanningBugTest
+    {
+        [Fact()]
+        public void ExecuteTest()
+        {
+            var fasesWithOffset = JsonConvert.DeserializeObject<Dictionary<Fase, bool>>(File.ReadAllText("FasesWithOffset.json"));
+            var biddingState = new BiddingState(fasesWithOffset);
+
+            // Controls --> Controls
+            biddingState.fase = Fase.Controls;
+            biddingState.UpdateBiddingState(3, Fase.Controls, "");
+            Assert.Equal(4, biddingState.nextBidIdForRule);
+
+            // Controls --> Scanning
+            biddingState.fase = Fase.Controls;
+            biddingState.UpdateBiddingState(6, Fase.Scanning, "");
+            Assert.Equal(0, biddingState.nextBidIdForRule);
+
+            // Scanning --> Scanning
+            biddingState.fase = Fase.Scanning;
+            biddingState.UpdateBiddingState(5, Fase.Scanning, "");
+            Assert.Equal(5, biddingState.nextBidIdForRule);
+        }
+    }
+}
