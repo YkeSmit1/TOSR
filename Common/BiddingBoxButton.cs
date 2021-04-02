99ac3d0b73c0a4ae0be01ef3c17357fe318c83d7
Yke Smit <44127043+YkeSmit1@users.noreply.github.com>
2021-04-02 15:43:44 +0200

    Moved some classes to common project

---
+++ Common/BiddingBoxButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace Common
+{
+    public class BiddingBoxButton : Button
+    {
+        public Bid bid;
+
+        public BiddingBoxButton(Bid bid)
+        {
+            this.bid = bid;
+        }
+    }
+}
